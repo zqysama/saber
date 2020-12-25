4ab2220a5223c6ca270bec87d0e45438b2f48822
周泉宇 <qyzhou@zjft.com>
2020-12-25 11:04:01 +0800

    feat: 新增lombok支持、链表操作练习

---
+++ algorithms/src/main/java/com/zqysama/algorithmsList/AlgorithmsOneList.java
@@ -0,0 +1,91 @@
+package com.zqysama.algorithmsList;
+
+public class AlgorithmsOneList {
+
+    private Node head;
+
+    private Node last;
+
+    private int size;
+
+    public void insert(int data, int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围");
+        }
+        Node insertedNode = new Node(data);
+        if (size == 0) {
+            head = insertedNode;
+            last = insertedNode;
+        } else if (index == 0) {
+            insertedNode.next = head;
+            head = insertedNode;
+        } else if (index == size) {
+            last.next = insertedNode;
+            last = insertedNode;
+        } else {
+            Node prevNode = get(index - 1);
+            insertedNode.next = prevNode.next;
+            prevNode.next = insertedNode;
+        }
+        size ++;
+    }
+
+    public Node remove(int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围");
+        }
+        Node removedNode = null;
+        if (index == 0) {
+            removedNode = head;
+            head = head.next;
+        } else if (index == size - 1) {
+            Node prevNode = get(index - 1);
+            prevNode.next = null;
+            removedNode = last;
+            last = prevNode;
+        } else {
+            removedNode = get(index);
+            Node prevNode = get(index - 1);
+            prevNode.next = removedNode.next;
+        }
+        size --;
+        return removedNode;
+    }
+
+    public void outPut() throws Exception {
+        Node temp = head;
+        while (temp != null) {
+            System.out.println(temp.data);
+            temp = temp.next;
+        }
+    }
+
+    public Node get(int index) throws Exception {
+        if (index < 0 || index > size) {
+            throw new IndexOutOfBoundsException("超出链表节点范围");
+        }
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        AlgorithmsOneList list = new AlgorithmsOneList();
+        list.insert(3,0);
+        list.insert(7,1);
+        list.insert(9,2);
+        list.insert(5,3);
+        list.insert(6,4);
+        System.out.println("init");
+        list.outPut();
+        list.remove(3);
+        System.out.println("remove1");
+        list.outPut();
+        list.remove(3);
+        System.out.println("remove2");
+        list.outPut();
+    }
+}
---
+++ algorithms/src/main/java/com/zqysama/algorithmsList/Node.java
@@ -0,0 +1,16 @@
+package com.zqysama.algorithmsList;
+
+/**
+ * 单向链表节点
+ * @author 周泉宇
+ */
+public class Node {
+
+    int data;
+
+    Node next;
+
+    public Node(int data) {
+        this.data = data;
+    }
+}
